4c87737dfd0fe1f4e3a561c2e15c3c4dbd3bddf7
huyensiukawaii <huyendnk1808@gmail.com>
2025-08-13 16:35:27 +0700

    Sửa FE itinerary detail + thêm FE activity

--- backend/src/activities/dto/update-activity.dto.ts
+++ backend/src/activities/dto/update-activity.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateActivityDto } from './create-activity.dto';
+import { IsString, IsOptional } from 'class-validator';
 
-export class UpdateActivityDto extends PartialType(CreateActivityDto) {}
+export class UpdateActivityDto extends PartialType(CreateActivityDto) {
+  @IsOptional()
+  @IsString()   
+  id?: string;
+}
